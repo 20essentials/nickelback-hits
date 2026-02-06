dac327713d8fc040cc985e736a3b6bd605820554
DominicDev <s0l0estudi4r@gmail.com>
2026-02-06 13:48:20 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'NICKELBACK';
+export let nameSongs = [
+	"HOW YOU REMIND ME", /*1*/
+	"PHOTOGRAPH", /*2*/
+	"ROCKSTAR", /*3*/
+	"SOMEDAY", /*4*/
+	"FAR AWAY", /*5*/
+	"BURN IT TO THE GROUND", /*6*/
+	"ANIMALS", /*7*/
+	"IF TODAY WAS YOUR LAST DAY", /*8*/
+	"NEVER GONNA BE ALONE", /*9*/
+	"SAVIN' ME", /*10*/
+	"WHEN WE STAND TOGETHER", /*11*/
+	"LULLABY", /*12*/
+	"SOMETHING IN YOUR MOUTH", /*13*/
+	"THIS AFTERNOON", /*14*/
+	"TOO BAD", /*15*/
+	"GOTTEN", /*16*/
+	"FEELIN' WAY TOO DAMN GOOD", /*17*/
+	"FIGURED YOU OUT", /*18*/
+	"SATURDAY NIGHT'S ALRIGHT", /*19*/
+	"HERO", /*20*/
+]
